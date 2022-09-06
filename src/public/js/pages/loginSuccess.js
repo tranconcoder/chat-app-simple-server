f2a24cf2ef493a033e501b179b68e08625d54b13
tranconcoder <tranconcoder@gmail.com>
2022-09-06 16:32:20 +0700

    redeploy

--- src/public/js/pages/loginSuccess.js
+++ src/public/js/pages/loginSuccess.js
@@ -1,10 +1,6 @@
 const profile = JSON.parse(document.getElementById('profile').dataset.profile);
 
-const urlList = [
-	'http://localhost:4000',
-	'http://192.168.28.67:4000',
-	'https://tranconcoder.github.io',
-];
+const urlList = ['https://tranconcoder.github.io'];
 
 urlList.forEach((url) => {
 	window.opener.postMessage(
